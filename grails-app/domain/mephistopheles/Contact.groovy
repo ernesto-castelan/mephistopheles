50b79a41f10c629e8f34950fb26722c04731d33c
jonathanNieto <jonathan.nieto.davila@gmail.com>
2018-09-17 13:42:33 -0500

    improvement: Clean code tasks

--- grails-app/domain/mephistopheles/Contact.groovy
+++ grails-app/domain/mephistopheles/Contact.groovy
@@ -1,7 +1,6 @@
 package mephistopheles
 
 class Contact {
-    static belongsTo = [user: User]
 
     String name
     String fatherLastName
@@ -14,8 +13,8 @@ class Contact {
         name size: 1..30
         fatherLastName size: 1..30
         motherLastName size: 1..30
-        mobilePhone matches: "([0-9]{2}[-]*){4}"
-        homePhone matches: "([0-9]{2}[-]*){4}"
+        mobilePhone matches: "[0-9]+", size: 8..12
+        homePhone matches: "[0-9]+", size: 8..12
         relationShip size: 3..25
     }
 
--- grails-app/domain/mephistopheles/User.groovy
+++ grails-app/domain/mephistopheles/User.groovy
@@ -22,21 +22,21 @@ class User {
     Boolean active
 
     static constraints = {
-        name size: 1..30, blank:false
-        fatherLastName size: 1..30, blank: false
+        name size: 1..30
+        fatherLastName size: 1..30
         motherLastName size: 1..30, nullable: true
         admissionDate max: new Date()
         birthDate max: new Date() - 6570
-        separationDate validator: { Date separationDate, User user-> (separationDate > (user.admissionDate + 30))}
-        homePhone matches: "([0-9]{2}[-]*){4}"
-        mobilePhone matches: "([0-9]{2}[-]*){4}"
+        separationDate validator: { Date separationDate, User user -> (separationDate > (user.admissionDate + 30))}
+        homePhone matches: "[0-9]+"
+        mobilePhone matches: "[0-9]+"
         ineNumber matches: "[0-9]{10}", unique: true
         socialSecurityNumber matches: "[0-9]{10}", unique: true
-        address size: 10..200, blank: false, unique: false
+        address size: 10..200
         personalEmail email:true, nullable: true, unique:true
         jobEmail email:true, unique:true
         bankAccountKey matches: "[0-9]{18}", unique: true
-        gitHubAccount size: 1..30, blank: false, unique: true
+        gitHubAccount size: 1..30, unique: true
     }
 
     @Override
